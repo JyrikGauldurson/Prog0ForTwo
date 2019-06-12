ff98023a526e4f05391dd5e245a3257ae4715a1d
Haooker in Debian <damieng@yeah.net>
2019-06-12 22:13:34 +0800

    o

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+#include<string>
+#include<memory>
 
+std::string* getf(std::string&& r){
+	return new std::string(std::move(r));
+}
 int main()
 {
 	using namespace std;
-	cout<<"OPAQUE\n";
+	string s("OPAQUE");
+	string* ps(getf(std::move(s)));
+	cout<<*ps<<endl;
+	delete ps;
 	return EXIT_SUCCESS;
 }
